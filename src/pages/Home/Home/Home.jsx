336c9b8dadb7e99dec5fa9ae649af61d842f09de
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-07-13 18:43:06 +0600

    create react vite app and add tailwind css and react router

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold mt-20">User Management System</h1>
+            <h1 className="text-5xl font-bold mt-20">All Users</h1>
+          <Link>
+          <button className="btn btn-secondary">Add User</button>
+          </Link>
+            <div className="">
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
